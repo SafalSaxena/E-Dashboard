05f5635eff9417242c8515e477fd323980755151
SafalSaxena <150792284+SafalSaxena@users.noreply.github.com>
2024-07-16 12:30:19 +0530

    E-Dashboard

---
+++ front-end/src/components/Nav.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Nav = () => {
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/signup");
+  };
+  return (
+    <div>
+      <img
+        alt="logo"
+        className="logo"
+        // src="https://yt3.googleusercontent.com/ytc/AIf8zZSqnIyhDK5VunVEGU72m8PM1zQyjziEvWD1w3sIyg=s900-c-k-c0x00ffffff-no-rj"
+        src="https://freevector-images.s3.amazonaws.com/uploads/vector/preview/36682/36682.png"
+      />
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update">Update Products</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link onClick={logout} to="/signup">
+              Logout ({JSON.parse(auth).name})
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Nav;
